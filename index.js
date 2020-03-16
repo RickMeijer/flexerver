dc9191573938b31332a20e5199deefed86267535
Rick Meijer <rickmeijer@gmail.com>
2020-03-16 16:41:21 +0100

    style: Formatting

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const { lstatSync } = require('fs');
-const { join } = require('path');
-const express = require('express');
+const { lstatSync } = require("fs");
+const { join } = require("path");
+const express = require("express");
 
 const app = express();
 let server;
 
 const isDirectory = source => lstatSync(source).isDirectory();
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   server.close();
-  process.exit('🔥 Killed it with fire');
+  process.exit("🔥 Killed it with fire");
 });
 
-process.on('exit', msg => {
+process.on("exit", msg => {
   console.log(msg);
 });
 
-const givenDir = process.argv[2] || '.';
+const givenDir = process.argv[2] || ".";
 const dir = join(process.cwd(), givenDir);
 if (!isDirectory(dir)) process.exit(`🤷‍♂️ ${dir} is not a valid directory`);
 
 app.use(express.static(dir));
-server = app.listen(1337, () => {
-  console.log(`🤖 Opening ${dir} on localhost:1337`);
+server = app.listen(80, () => {
+  console.log(`🤖 Opening ${dir} on localhost:80`);
 });
