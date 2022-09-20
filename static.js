9ecf569bbe1500b34840a864318b6feb432e66e4
Rick Meijer <info@rickmeijer.net>
2022-09-20 16:34:32 +0200

    feat: adds more flexibility for static serve

--- static.js
+++ static.js
@@ -5,14 +5,14 @@ const express = require("express");
 const app = express();
 let server;
 
-const isDirectory = source => lstatSync(source).isDirectory();
+const isDirectory = (source) => lstatSync(source).isDirectory();
 
 process.on("SIGINT", () => {
   server.close();
   process.exit("🔥 Killed it with fire");
 });
 
-process.on("exit", msg => {
+process.on("exit", (msg) => {
   console.log(msg);
 });
 
@@ -20,7 +20,8 @@ const givenDir = process.argv[2] || ".";
 const dir = join(process.cwd(), givenDir);
 if (!isDirectory(dir)) process.exit(`🤷‍♂️ ${dir} is not a valid directory`);
 
+const givenPort = process.argv[3] || 80;
 app.use(express.static(dir));
-server = app.listen(80, () => {
-  console.log(`🤖 Opening ${dir} on localhost:80`);
+server = app.listen(givenPort, () => {
+  console.log(`🤖 Opening ${dir} on localhost:${givenPort}`);
 });
