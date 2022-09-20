5d532334270d3c776fc750e17117e942a868cca7
Rick Meijer <info@rickmeijer.net>
2022-09-20 16:34:49 +0200

    feat: simple proxy

--- proxy.js
+++ proxy.js
@@ -1,51 +1,30 @@
 const express = require("express");
-const axios = require("axios");
-const https = require("https");
-const cors = require("cors")();
+const morgan = require("morgan");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const proxyAddr = {
-  // port: url
-  [process.argv[3] || 80]: process.argv[2]
-};
+// Create Express Server
+const app = express();
 
-const agent = new https.Agent({
-  rejectUnauthorized: false
+// Configuration
+const PORT = process.argv[3];
+const HOST = "localhost";
+const API_SERVICE_URL = process.argv[2];
+
+// Logging
+app.use(morgan("dev"));
+app.use(cors());
+
+// Proxy endpoints
+app.use(
+  "/*",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
-
-let server = [];
-
-Object.keys(proxyAddr).forEach(port => {
-  const app = express();
-  const address = proxyAddr[port];
-  app.use(cors);
-  app.options("*", cors);
-
-  app.use("/*", (req, res) => {
-    let url = `${address}/${req.params[0]}`;
-    console.log(`🙅🏽‍♂️ proxy calling ${url}`);
-
-    axios({
-      method: req.method,
-      url,
-      httpsAgent: agent
-    })
-      .then(response => {
-        console.log("response has come");
-
-        res.send(response.data);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(err.response.status).send(err.response.statusText);
-      });
-  });
-  server.push(
-    app.listen(port, () =>
-      console.log(`🤖 Routing requests from localhost:${port} to ${address}`)
-    )
-  );
-});
-
-process.on("SIGINT", () => server.forEach(s => s.close()));
-
-process.on("exit", () => console.log("🔥 Killed it with fire"));
